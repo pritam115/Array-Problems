3cb942825965fdb3d785795e339ca0794c3bfc9d
Pritam Saha Turja <110053926+pritam115@users.noreply.github.com>
2022-08-20 00:20:35 +0600

    Add files via upload

---
+++ 2d array user input.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    int a[3][4];
+    for (i=0; i<3; i++)
+    {
+        for(j=0; j<4; j++)
+        {
+            printf("a[%d][%d]=  ", i,j);
+            scanf("%d", &a[i][j]);
+        }
+        printf("\n");
+    }
+    for (i=0; i<3; i++)
+    {
+        for(j=0; j<4; j++)
+        {
+            printf(" %d ", a[i][j]);
+        }
+        printf("\n");
+    }
+}
+
